3d8565f4f1fb639a9bc35d3b61af91ad1d17f384
Albert Florinus <abbeflorinus@gmail.com>
2021-05-09 15:15:30 +0200

    2

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import pandas as pd
 from tkinter.filedialog import askopenfilename
 import numpy as np
 import datetime
+import math
 
 class Weather():
     def __init__(self, start_date, end_date):
@@ -18,16 +19,43 @@ class Weather():
         self.df2 = self.df.copy().iloc[:, 1:4]
         self.df2 = self.df2.applymap(f)
         self.df2["Date"] = self.df1
-        self.df = self.df2.reindex(columns=['Date',"TempHighF","TempAvgF","TempLowF"])
+        self.df = self.df2.reindex(columns=['Date',"TempHighF","TempAvgF","TempLowF"])  
+        
+
+    def criterias(self, df, temp, active_time = "day"):
+
+        conf = lambda x: 1-x*0.04
+
+        link = {"day": "TempHighF", "night": "TempLowF", "all_day": "TempAvgF"}
+
+        test = df.to_numpy()
+    
+        storer = []
+  
+        for i in range(0, len(test)):
+            storer.append(i)
+
+        storer = [*map(conf, storer)]
+
+        df["acc"] = storer
+
+        self.suggest = self.df.loc[ self.df[ link[active_time] ] > temp]
+
+        self.suggest["score"] = (self.suggest[ link[active_time] ]   ) * self.suggest["acc"] * 2
+
+        self.suggest = self.suggest.sort_values(by ='score', ascending = False)
+
+
+
 
     
 
 def main():
-    #start_date = pd.to_datetime("2014-02-23", format = '%Y-%m-%d')
-    #end_date = pd.to_datetime("2014-03-1", format = '%Y-%m-%d')
-    abbe = Weather("2014-02-23", "2014-03-2")
-    abbe.celcius_conv()
-    print(abbe.df)
+    inspo = Weather("2016-05-25", "2016-06-2")
+    inspo.celcius_conv()
+    inspo.criterias(inspo.df, 20)
+
+    print(inspo.suggest)
 
 if __name__ == "__main__":
     main()
